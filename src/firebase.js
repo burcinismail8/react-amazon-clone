e978aa7424babb4386fcab9b27f3c8417ba248b0
Burchin Ismail <burcinismail8@gmail.com>
2022-08-10 14:12:15 +0300

    Added nav menu

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDie63hPREIkNLYg-GwJ7MXaNBZa2XTKRw",
+  authDomain: "clone-ada98.firebaseapp.com",
+  projectId: "clone-ada98",
+  storageBucket: "clone-ada98.appspot.com",
+  messagingSenderId: "767308803851",
+  appId: "1:767308803851:web:995b4c9fadd0a624d57c0e",
+};
