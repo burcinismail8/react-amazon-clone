813937bc4370493f7fab50ef831529522b61bf51
Burchin Ismail <burcinismail8@gmail.com>
2022-08-14 11:31:45 +0300

    Created login page

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import {
 
 import "./App.css";
 import Checkout from "./Checkout";
-import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/Checkout.js
+++ src/Checkout.js
@@ -3,36 +3,40 @@ import "./Checkout.css";
 import Subtotal from "./Subtotal";
 import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
+import Header from "./Header";
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useStateValue();
   console.log(basket);
   return (
-    <div className="checkout">
-      <div className="checkout__left">
-        <img
-          className="checkout__ad"
-          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-          alt=""
-        />
-        <div>
-          <h2 className="checkout__title">Your shopping Basket</h2>
-          {basket.map((item, id) => (
-            <CheckoutProduct
-              key={id}
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+    <>
+      <Header />
+      <div className="checkout">
+        <div className="checkout__left">
+          <img
+            className="checkout__ad"
+            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+            alt=""
+          />
+          <div>
+            <h2 className="checkout__title">Your shopping Basket</h2>
+            {basket.map((item, id) => (
+              <CheckoutProduct
+                key={id}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
+        <h2 className="checkout__right">
+          <Subtotal />
+        </h2>
       </div>
-      <h2 className="checkout__right">
-        <Subtotal />
-      </h2>
-    </div>
+    </>
   );
 };
 
